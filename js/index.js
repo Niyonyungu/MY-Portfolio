3433330b0d574d96af33c3670d1b08ebff6ab90c
Michel vainqueur <vainqueurmg@gmail.com>
2023-09-05 21:07:13 +0300

    update

--- js/index.js
+++ js/index.js
@@ -30,15 +30,3 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SENDING EMAIL ON FORM ===============*/
-
-function sendEmail() {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "vainqueurmg@gmail.com",
-    Password: "E06C7F2C5417D4A3FB7B8B4CE4DFA72C92FD",
-    To: "vainqueurmg@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "New Contact Form Message From Your Portfolio",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
